445b9c15aef0bef5785fba0db51b3f7f0f9fdf53
filipefilardi <filipefilardi@gmail.com>
2026-05-05 21:59:54 -0300

    Implement macOS support and update project configurations
    - Added macOS support to the project, including necessary files and configurations for the Runner target.
    - Updated the app's bundle identifier to use "com.silent_summit.altcast" for consistency.
    - Modified the pubspec.yaml to enable macOS icon generation and adjusted Android launcher icon settings.
    - Introduced new files for macOS-specific configurations, entitlements, and UI elements, enhancing the overall project structure.
    - Included various assets for the macOS app icon set to ensure proper branding across platforms.

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,26 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import connectivity_plus
+import flutter_secure_storage_darwin
+import media_kit_libs_macos_video
+import media_kit_video
+import package_info_plus
+import screen_brightness_macos
+import sqflite_darwin
+import wakelock_plus
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
+  MediaKitLibsMacosVideoPlugin.register(with: registry.registrar(forPlugin: "MediaKitLibsMacosVideoPlugin"))
+  MediaKitVideoPlugin.register(with: registry.registrar(forPlugin: "MediaKitVideoPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  ScreenBrightnessMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenBrightnessMacosPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
+}
